7ceee069977af85d9543cce07920031f928cefc8
sumit27vishwa <117379693+sumit27vishwa@users.noreply.github.com>
2023-04-15 19:57:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,23 +5,42 @@ let arr = [
   { id: 2, name: "jack", age: "20", profession: "developer" },
   { id: 3, name: "karen", age: "19", profession: "admin" },
 ];
+let brr = [
+  { id: 4, name: "janardan", age: "22", profession: "coder" },
+];
 
 function PrintDeveloperbyMap() {
-  //Write your code here , just console.log
+  for(let i=0;i<arr.length;i++){
+  if(arr.map(profession)="developer"){
+     console.log(arr[i]);
+   }
+  } //Write your code here , just console.log
 }
 
 function PrintDeveloperbyForEach() {
+  for(let i=0;i<arr.length;i++){
+  if(arr.ForEach(profession)="developer"){
+     console.log(arr[i]);
+   }
+  }
   //Write your code here , just console.log
 }
 
 function addData() {
+  arr.push({id:4,name:"susan",age:"20",profession:"intern"});
+  console.log(arr);
   //Write your code here, just console.log
 }
 
 function removeAdmin() {
+  const itemToBeRemoved ={ id: 3, name: "karen", age: "19", profession: "admin" };
+  arr.splice(arr.findIndex(a => a.id === itemToBeRemoved.id) ,1);
+  console.log(arr);
   //Write your code here, just console.log
 }
 
 function concatenateArray() {
+  const children = arr.concat(brr);
+  console.log(children);
   //Write your code here, just console.log
 }
